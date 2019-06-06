d1f8f6f32accd8b16c9af2d7c582ad7efcbf3db2
Iraldo Arévalo Delfín <15159678+iraldoad@users.noreply.github.com>
2019-06-06 09:39:52 -0500

    Update helpers.php

--- src/Dd/helpers.php
+++ src/Dd/helpers.php
@@ -5,14 +5,10 @@ use Symfony\Component\VarDumper\VarDumper;
 if (! function_exists('dd')) {
 	function dd ()
 	{
-        foreach (func_get_args() as $v) {
-            VarDumper::dump($v);
-        }
+	foreach (func_get_args() as $v) {
+	    VarDumper::dump($v);
+	}
 
-        if (1 < func_num_args()) {
-            return func_get_args();
-        }
-
-        die(1);
+	die;
 	}
 }

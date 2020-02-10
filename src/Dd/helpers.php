601d5e17cf55428e6963252b055d7c561dab9f28
Iraldo <iraldo.arevalo@experteam.com.ec>
2020-02-10 11:39:55 -0500

    Bug fix for older PHP versions

--- src/Dd/helpers.php
+++ src/Dd/helpers.php
@@ -3,8 +3,10 @@
 use Symfony\Component\VarDumper\VarDumper;
 
 if (!function_exists('dd')) {
-    function dd(...$vars)
+    function dd($vars)
     {
+        $vars = func_get_args();
+        
         foreach ($vars as $v) {
             VarDumper::dump($v);
         }
